90d39e62321aac8faadbe7fa00f6240da4d39c3c
Claudia Friedrich <claudae_f@hotmail.com>
2018-11-28 23:01:09 +0100

    change navigation close and back-button

--- flightjournal-react/src/components/header/navigation.js
+++ flightjournal-react/src/components/header/navigation.js
@@ -39,36 +39,59 @@ class Navigation extends Component {
     render() {
         //if Landingpage is home and the user is authorised show the hole navigation, else just show logout/login
         let isHome = (this.props.location.pathname === '/' || this.props.location.pathname === routes.HOME) ? true : false;
-        return (
-            <div className="header__menu">
-                <nav className="main-nav">
-                    {this.state.authUser && isHome ?
-                    <ul className="main-nav__wrapper">
+        let isForm = (this.props.location.pathname === routes.FLUGDATEN_ERFASSEN || this.props.location.pathname === routes.STARTPLATZ_ERFASSEN) ? true : false;
+        let navi;
+
+        if(!this.state.authUser){
+            navi = <ul className="main-nav__wrapper main-nav__wrapper--single">
+                      <li className="main-nav__link">
+                          <Link to={routes.LOGIN}>Login</Link>
+                       </li>
+                    </ul>
+        }else if(this.state.authUser && isHome){
+            navi = <ul className="main-nav__wrapper">
                         <li className="main-nav__link">
                             <Scrollchor to="flugplanung">Flugplanung</Scrollchor>
                         </li>
                         <li className="main-nav__link">
                             <Scrollchor to="fluege">Flüge</Scrollchor>
                         </li>
-                        {/* <li className="main-nav__link">
+                        <li className="main-nav__link">
                             <Scrollchor to="startplaetze">Startplätze</Scrollchor>
-                        </li> */}
-                        <li className="main-nav__link">
-                            <Link onClick={() => {this.doLogOut();}} to={routes.LANDING}>Logout</Link>
-                         </li>
-                    </ul>
-                            :
-                    <ul className="main-nav__wrapper main-nav__wrapper--single">
-                    {this.state.authUser ?
-                        <li className="main-nav__link">
-                            <Link onClick={() => {this.doLogOut();}} to={routes.LANDING}>Logout</Link>
-                        </li> :
-                        <li className="main-nav__link">
-                            <Link to={routes.LOGIN}>Login</Link>
                         </li>
-                        }
+                        <li className="main-nav__link">
+                            <Link onClick={() => {this.doLogOut();}} to={routes.LANDING}>Logout</Link>
+                        </li>
+                    </ul>;
+        }else if(this.state.authUser && isForm){
+            navi = <ul className="main-nav__wrapper main-nav__wrapper--single">
+                        <li className="main-nav__link">
+                            <Link className="main-nav__link-close-icon" to={routes.HOME}>
+                            <div className="main-nav__icon-text">Abbrechen</div>
+                                <div className="main-nav__icon">
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </Link>
+                        </li> 
                     </ul>
-                        }
+        }else{
+            navi = <ul className="main-nav__wrapper main-nav__wrapper--single">
+                        <li className="main-nav__link">
+                            <Link className="main-nav__link-back-icon" to={routes.HOME}>
+                                <div className="main-nav__icon-text">Zurück zur Übersicht</div>
+                                <div className="main-nav__icon">
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </Link>
+                        </li> 
+                    </ul>
+        }
+        return (
+            <div className="header__menu">
+                <nav className="main-nav">
+                    {navi}
                 </nav>
              </div>
         );
